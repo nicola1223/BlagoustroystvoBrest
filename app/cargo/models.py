b14f36c38b237c84b7de34db2cf2441b99783914
nicola1223 <belchuk2005@mail.ru>
2024-04-21 09:05:43 +0300

    Added model for cargo

--- app/cargo/models.py
+++ app/cargo/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+
+class Trucks(models.Model):
+    name = models.CharField(max_length=30)
+    price = models.FloatField()
+
+    def __str__(self):
+        return self.name
